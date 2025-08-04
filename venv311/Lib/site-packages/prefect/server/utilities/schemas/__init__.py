25a37648e7df4e32f3accd0c212994d9d79ea73e
Shruti Mahadik <shrutimahadik2102@gmail.com>
2025-08-04 12:48:20 +0530

    Removed virtual environment from repo and updated .gitignore

--- venv311/Lib/site-packages/prefect/server/utilities/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from .bases import (
-    PrefectBaseModel,
-    IDBaseModel,
-    ORMBaseModel,
-    ActionBaseModel,
-    get_class_fields_only,
-)
-from .fields import DateTimeTZ
-from prefect._internal.pydantic import HAS_PYDANTIC_V2
